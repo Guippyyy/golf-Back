e14de45b74a5c1d05c568f048c120b9af37b32a7
GUILLAUME DE CRAENE <guillaume.decraene@student.hogent.be>
2022-12-12 23:22:01 +0100

    asdf

--- prisma/generateClient.js
+++ prisma/generateClient.js
@@ -2,7 +2,7 @@ const { PrismaClient } = require("@prisma/client");
 
 let DATABASE_URL = "mysql://185375gd:Nkmo6wL5SMSBjs97fHib@vichogent.be:40043/185375gd?connection_limit=5&socket_timeout=3"
 if (process.env.NODE_ENV === "production") {
-    DATABASE_URL = `mysql://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}?connection_limit=5&socket_timeout=3`
+    DATABASE_URL = `mysql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}?connection_limit=5&socket_timeout=3`
 }
 
 const prisma = new PrismaClient({

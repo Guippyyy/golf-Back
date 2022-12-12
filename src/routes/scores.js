3db6bc7a470dfbad772792da1ef99303b881dea2
GUILLAUME DE CRAENE <guillaume.decraene@student.hogent.be>
2022-12-13 00:06:11 +0100

    auth weg

--- src/routes/scores.js
+++ src/routes/scores.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const prisma = require("../../prisma/generateClient");
 const { addScore, getScore, deleteScore } = require('../services/scores');
-const {check, validationResult} = require('express-validator')
-const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
+const {check} = require('express-validator')
+const { auth} = require('express-oauth2-jwt-bearer');
 const checkJwt = auth({
     audience: 'https://golfApp.guillaume.hogent.be',
     issuerBaseURL: `https://dev-8cpbbh21w2gsf8yo.us.auth0.com/`,
@@ -16,7 +15,7 @@ router.post('/', [
     check('result').notEmpty()
 ], addScore);
 
-router.get('/', checkJwt, getScore);
+router.get('/', getScore);
 router.delete('/', deleteScore);
 
 module.exports = router;
